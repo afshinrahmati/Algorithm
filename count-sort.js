d84449cc853223e4aadeedb26435e2213c543df5
a.rahmati <a.rahmati@kianiranian.com>
2023-04-15 19:26:11 +0330

    sort Count Sort

--- count-sort.js
+++ count-sort.js
@@ -1,17 +1,26 @@
 var countingSort = function (arr) {
-    var counts = [];
-    var max = +Math.max(...arr); // get Max number
-    //   give  to all val ziro
-   
-    for (var i = 0; i < max; i++) {
-        counts[i] = 0;
+  var counts = [];
+  var max = +Math.max(...arr); // get Max number
+  //   give  to all val ziro
+
+  for (var i = 0; i < arr.length; i++) {
+    counts[i] = 0;
+  }
+  //   sum loop number
+  for (var i = 0; i < arr.length; i++) {
+    counts[arr[i]] += 1;
+  }
+
+  let sort_data = [];
+  for (let j = 0; j < max + 1; j++) {
+    while (counts[j] > 0) {
+      sort_data.push(j);
+      counts[j]--;
     }
-    //   sum loop number
-    for (var i = 0; i < max; i++) {
-        counts[arr[i]] += 1;
-    }
-    console.log(counts);
-    return counts;
+  }
+  return sort_data;
 };
-var result = countingSort([1, 2, 1, 2, 5, 2, 10,5]);
+const arr = [0, 5, 1, 2, 3, 1, 2, 4, 1, 5, 2];
+
+const result = countingSort(arr);
 console.log(result);
