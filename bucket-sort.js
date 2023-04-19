b77c1278bff34cbee09d166f4d5988ce14bf3659
a.rahmati <a.rahmati@kianiranian.com>
2023-04-19 19:00:40 +0330

    bucketSort

---
+++ bucket-sort.js
@@ -0,0 +1,10 @@
+let a = [74, 5, 8, 7, 9, 52, 14, 63, 2, 12, 3, 1];
+let b = [];
+for (let i = 0; i < a.length; i++) {
+  b.push([]);
+}
+b[5] = [3];
+// for (let i = 0; i < a.length; i++) {
+//   b[i / 10] = [i];
+// }
+console.log(b);
