b8c38ab6325aee94e0550afb23b59d24edc6ceab
a.rahmati <afshinrahmati93@gmail.com>
2024-09-23 09:37:47 +0330

    feat: :art: linkList

--- link-list/link-list.ts
+++ link-list/link-list.ts
@@ -12,7 +12,7 @@ class NodeListt<T> {
 class LinkList<T> {
     head: NodeListt<T> | null = null
 
-    addLast(val:T) {
+    addLasOrAdd(val:T) {
         const newNode = new NodeListt(val);
         // the first itema
         if(this.head == null) {
@@ -32,19 +32,71 @@ class LinkList<T> {
     }
     // the first node was now it come in next new node
     addFirst(val:T) {
+    // first create node
+    const newNode = new NodeListt(val)
 
-        const newNode = new NodeListt(val)
-        if(this.head == null) {
-            this.head = newNode
-        }else {
-            let current = this.head
-            while(current.next == null) {
-                
+    if(this.isEmpty()) {
+        this.head = newNode;
+    }else {
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+    }
+
+    indexOf(val:string | number) {
+        let current = this.head;
+        let i = 0;
+        while(current?.next != null){
+            if(current?.value == val) return i;
+            current = current?.next;
+            i++;
+        }
+        return -1;
+    }
+
+
+    contains(val:number) {
+        return this.indexOf(val) != -1;
+    }
+
+
+    removeFirst() {
+
+        if(this.isEmpty()) {
+            return "no item"
+        }
+        if(this.head?.next == null){
+            this.head = null;
+            return
+        }
+        const second =  this.head?.next;
+        this.head.next = null;
+        this.head = second;
+        
+    }
+    removeLast() {
+        let current = this.head
+        while(current?.next != null) {
+           
+            if(current.next.next == null){
+                current.next = null;
             }
+            current = current.next
         }
     }
+    private isEmpty() {
+     return this.head == null;
+    }
+
+    
 }
-// const l1 = new LinkList<number>()
-// l1.addLast(50)
-// l1.addLast(51)
-// l1.addLast(52)
+const l1 = new LinkList<number>()
+l1.addLasOrAdd(50)
+l1.addFirst(49)
+
+
+// l1.removeFirst()
+// console.log(l1)
+l1.removeLast()
+console.log(l1)
+// console.log(l1.indexOf(52));
