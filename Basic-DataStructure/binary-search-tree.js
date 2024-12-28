e8c26f357836d38539cbb4379a6751e8833fb7dd
afshinrahmati <afshinrahmati93@gmail.com>
2024-12-28 16:37:51 +0330

    feat: binary tree code

--- Basic-DataStructure/binary-search-tree.js
+++ Basic-DataStructure/binary-search-tree.js
@@ -7,23 +7,85 @@ class Node {
     }
 }
 
-class BinarySerachTree {
+class BinarySearchTree {
 
-    constructor(val) {
+    constructor() {
         this.root = null;
     }
-
+    // alll the time inset with the number max and mim in the code
     insert(value) {
         const newNode = new Node(value);
 
         if (this.root == null) {
             this.root = newNode;
-        } else {
-            let current = this.root;
-            while (true) {
-
-
+            return this;
+        }
+        let current = this.root;
+        console.log("thereeeeeee", value, current.value, "thereeeeeee")
+        while (true) {
+            if (value < current.value) {
+                // if no create new
+                if (!current.left) {
+                    current.left = newNode;
+                    return this;
+                }
+                //  if yes; current equal left if exist beacuse it on the while true
+                current = current.left;
+            } else if (value > current.value) {
+                if (!current.right) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            } else {
+                // Duplicate values are not allowed
+                return this;
+            }
+        }
+    }
+    search(value) {
+        let current = this.root;
+        if (!current) {
+            return false;
+        }
+        while (current) {
+            if (current.value == value) {
+                return true;
+            }
+            if (value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+    // the value on the right will become parent search on the net
+    delete(value, node = this.root, parent = null) {
+        if (!node) {
+            return false;
+        }
+        if (value < node.value) {
+            return this.delete(value, node.left, node);
+        } else if (value > node.value) { 
+            return this.delete(value,node.right,node);
+        } else if(value == node.value) {
+            // if it is parent
+            if(node.left && node.right) {
+                return this.delete(value,)
             }
         }
     }
 }
+
+
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+bst.insert(18);
+
+const find = bst.search(85);
+console.log(find)
